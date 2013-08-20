24d124345a241ff4cef675419ef44faeac1c71e8
Joseph Wilk <joe@josephwilk.net>
2013-08-20 17:13:02 +0200

    naming

--- src/hystrix_event_stream_clj/core.clj
+++ src/hystrix_event_stream_clj/core.clj
@@ -1,4 +1,4 @@
-(ns hystrix-event-stream-clj.response
+(ns hystrix-event-stream-clj.core
   (:require
    [cheshire.core :as json]
    [aleph.http  :refer :all]
@@ -16,17 +16,18 @@
     (catch Exception e
       false)))
 
-(defn- metric-streaming [ch] (future (loop [connected true]
-                                      (when connected
-                                        (Thread/sleep 1000)
-                                        (recur (write-metrics ch))))))
+(defn- metric-streaming [ch]
+  (future
+    (loop [connected true]
+      (Thread/sleep 1000)
+      (when connected (recur (write-metrics ch))))))
 
 (defn- init-stream-channel [ch]
   (receive-all ch (fn [_]))
   (metric-streaming ch))
 
-(defn stream-metrics []
-  (let [ch (named-channel :metric-stream init-stream-channel)]
+(defn stream []
+  (let [ch (named-channel :hystrix-metric-stream init-stream-channel)]
     {:status 200 :body ch :headers {"Content-Type" "text/event-stream;charset=UTF-8"
                                     "Cache-Control" "no-cache, no-store, max-age=0, must-revalidate"
                                     "Pragma" "no-cache"}}))
