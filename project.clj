808b0ab2e4fcd5947c43e1e140ec6b8637b393fd
josephwilk <joe@josephwilk.net>
2013-08-21 14:49:07 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject hystrix-event-stream-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject hystrix-event-stream-clj "0.1.0"
+  :description "Generate hystrix event streams"
+  :url "http://github.com/josephwilk/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
