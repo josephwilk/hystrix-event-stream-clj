a063c9f667cae387d5c28e74161ed6d3e75c6997
josephwilk <joe@josephwilk.net>
2013-08-21 14:51:14 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hystrix-event-stream-clj "0.1.0"
+(defproject hystrix-event-stream-clj "0.1.1"
   :description "Generate hystrix event streams"
   :url "http://github.com/josephwilk/"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.netflix.hystrix/hystrix-clj "1.3.1"]
                  [com.netflix.hystrix/hystrix-metrics-event-stream "1.1.2"]
+                 [lamina "0.5.0-rc3"]
                  [aleph "0.3.0-rc2"]
                  [cheshire "5.2.0"]]
 
